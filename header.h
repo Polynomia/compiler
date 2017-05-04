51b05162940fd0356c8ce26720948703d90eb061
Polynomia <wyrsjtu@163.com>
2017-05-04 19:54:09 +0800

    finish input

--- header.h
+++ header.h
@@ -5,16 +5,15 @@
 #include<string.h>
 #include <vector>
 #include <algorithm>
-
+#include<set>
 using namespace std;
 
 class grammar
 {
 public:
 	string left;
-	vector<string> right;
+	vector<set<string> > right;
 	bool epsilon();
-//private:
 	grammar(){}
 	grammar(string s){left=s;}
 	~grammar(){}
@@ -22,8 +21,63 @@ public:
 
 bool grammar::epsilon()
 {
-	 vector<string>::iterator result = find(right.begin( ), right.end( ), '#' );
-	if (result==right.end())
-		return false;
-	else return true; 
+	 set<string>::iterator result;
+	int i;
+	for(i=0;i<right.size();i++){
+	result=right[i].find("#");
+	if (result!=right[i].end())
+		return true;
+	 }
+	return false;
 }
+
+
+	map<string,int>dic;
+	vector<grammar>vnset;
+void readtxt(){	
+	ifstream ifile;
+	ifile.open("input.txt");
+	set<string> right1;
+	string str,s,s2;
+	char line[512];
+	int size,i,num,j;
+	while(ifile.getline(line,512)){
+		str=string(line);
+		size=str.length();
+		for(i=0;i<size;i++){
+			if(isblank(str[i])) continue;
+			if(str[i]=='-') break;
+			s+=str[i];
+		}
+		if(!dic[s]){
+			vnset.push_back(s);
+			dic[s]=vnset.size()-1;
+		}
+		num=dic[s];
+		j=0;
+		int x=1;
+		while(true){
+		if(str[i]=='-'||str[i]=='>'||isblank(str[i])) i++;
+		else break;}
+		vnset[num].right.push_back(right1);
+		for(;i<size;i++){
+			if(str[i]=='|') {
+				if(x) vnset[num].right[j].insert(s2);
+				s2.clear();right1.clear();j++;
+				vnset[num].right.push_back(right1);
+				continue;}
+			if(isblank(str[i])){
+				s2.clear();continue;	
+			}
+			x=1;
+			s2+=str[i];
+				if(isblank(str[i+1])) 
+			{vnset[num].right[j].insert(s2);
+				x=0;}
+		}
+		vnset[num].right[j].insert(s2);
+		s2.clear();
+		right1.clear();
+		s.clear();
+}}
+
